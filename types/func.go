75a15951dafaf06265e571761ae7c93fddcb68d6
Otto Schuldt <ottoschuldt@gmail.com>
2021-09-19 22:56:03 +0200

    updates dummy func

--- types/func.go
+++ types/func.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func OnlyCertainNumbers(n int64) (int64, error) {
 	switch n {
-	case 200, 300, 1505, 45450:
+	case 300, 1505, 15000:
 		return 0, fmt.Errorf("invalid number %d", n)
+	case 200:
+		panic("BOOM")
 	}
 	return n, nil
 }
